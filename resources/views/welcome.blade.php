7d3ee98638f37567baef6c8596eb66cffe1265e6
brand <badamianus@gmail.com>
2026-02-06 10:01:51 +0800

    Add a logo for the tab section in the browser

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{ asset('images/tungma-logo-3-removebg-preview.png') }}" type="image/png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
